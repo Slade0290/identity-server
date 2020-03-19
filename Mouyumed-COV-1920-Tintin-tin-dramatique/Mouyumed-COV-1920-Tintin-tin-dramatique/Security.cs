4627ff8cd8347a97ae5a8c2734f552fd9190560d
antoine <marteantoine@gmail.com>
2020-03-19 17:02:34 +0100

    security

--- Mouyumed-COV-1920-Tintin-tin-dramatique/Mouyumed-COV-1920-Tintin-tin-dramatique/Security.cs
+++ Mouyumed-COV-1920-Tintin-tin-dramatique/Mouyumed-COV-1920-Tintin-tin-dramatique/Security.cs
@@ -10,27 +10,53 @@ namespace Mouyumed_COV_1920_Tintin_tin_dramatique
 		{
 		}
 
-		public string hashPassword(string pw)
+		public int hashPassword(String pw)
 		{
+
 			return pw.GetHashCode();
+
 		}
 
 		public Boolean passwordCheck(string pw,string username)
 		{
-			string testPwd = hashPassword(pw);
-			string getPwd = getPassword(username);
+			int testPwd = hashPassword(pw);
+			int getPwd = getPassword(username);
 
-			return Compare(testPwd, getPwd);
+			return (testPwd.CompareTo(getPwd) != 0) ? true : false;
 		}
 
 		public Boolean saveToken(string token, string username)
 		{
 
+			System.Data.SqlClient.SqlConnection sqlConnection1 =
+			new System.Data.SqlClient.SqlConnection("YOUR CONNECTION STRING");
+
+			System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+			cmd.CommandType = System.Data.CommandType.Text;
+			cmd.CommandText = "UPDATE User (Username,TOKEN ) VALUES (@username, @token)";
+			cmd.Connection = sqlConnection1;
+
+			sqlConnection1.Open();
+			cmd.ExecuteNonQuery();
+			sqlConnection1.Close();
+			return true;
 		}
 
-		public string getPassword(string username)
+		public int getPassword(string username)
 		{
+			int pwd;
+			System.Data.SqlClient.SqlConnection sqlConnection1 =
+			new System.Data.SqlClient.SqlConnection("YOUR CONNECTION STRING");
 
+			System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+			cmd.CommandType = System.Data.CommandType.Text;
+			cmd.CommandText = "Select password FROM USER WHERE username = @usename";
+			cmd.Connection = sqlConnection1;
+
+			sqlConnection1.Open();
+			cmd.ExecuteNonQuery();
+			sqlConnection1.Close();
+			return pwd;
 		}
 
 		public string createToken(int size)
@@ -40,12 +66,29 @@ namespace Mouyumed_COV_1920_Tintin_tin_dramatique
 
 		public Boolean tokenIsExpirated()
 		{
+			System.Data.SqlClient.SqlConnection sqlConnection1 =
+			new System.Data.SqlClient.SqlConnection("YOUR CONNECTION STRING");
+
+			System.Data.SqlClient.SqlCommand cmd = new System.Data.SqlClient.SqlCommand();
+			cmd.CommandType = System.Data.CommandType.Text;
+			cmd.CommandText = "Select tokenDateLimite FROM USER WHERE username = @usename";
+			cmd.Connection = sqlConnection1;
+
+			sqlConnection1.Open();
+			cmd.ExecuteNonQuery();
+			sqlConnection1.Close();
+
+
+			//DateTime d1 = DateTime.Now; 
+			//d2 = Date in base
+			//DateTime.Compare(d1, d2)
+			return (DateTime.Compare(d1, d2)>=0)?true:false;
 
 		}
 
 		public void tokenRefresh()
 		{
-
+			createToken(12);
 		}
 	}
 
