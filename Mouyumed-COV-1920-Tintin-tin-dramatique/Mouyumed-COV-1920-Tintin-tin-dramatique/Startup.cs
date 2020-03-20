5c9db5f0fdba49bcdd4a5442c2bfaef8129fcfb5
Slade0290 <ly.david@live.fr>
2020-03-20 17:17:09 +0100

    fix errors - one left

--- Mouyumed-COV-1920-Tintin-tin-dramatique/Mouyumed-COV-1920-Tintin-tin-dramatique/Startup.cs
+++ Mouyumed-COV-1920-Tintin-tin-dramatique/Mouyumed-COV-1920-Tintin-tin-dramatique/Startup.cs
@@ -1,13 +1,9 @@
-using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
+using identity.Data;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.AspNetCore.Http;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using identity.Data;
 
 namespace Mouyumed_COV_1920_Tintin_tin_dramatique
 {
