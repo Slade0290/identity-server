edfbb9e2e81c974c0403495a1b1f33224de047b8
Florian SAUMUROT <florian.saumurot@gmail.com>
2020-03-20 11:19:26 +0100

    database class done

--- Mouyumed-COV-1920-Tintin-tin-dramatique/Mouyumed-COV-1920-Tintin-tin-dramatique/Database.cs
+++ Mouyumed-COV-1920-Tintin-tin-dramatique/Mouyumed-COV-1920-Tintin-tin-dramatique/Database.cs
@@ -1,21 +1,30 @@
 ﻿using System;
+using System.Data;
+using System.Data.SqlClient;
 
 namespace Mouyumed_COV_1920_Tintin_tin_dramatique
 {
 	public class Database
 	{
-		public Database()
+		private SqlConnection connection;
+		public Database(string datasource, string userid, string pwd, string table)
 		{
+			init(datasource, userid, pwd, table);
 		}
 
-		public void init()
+		public void init(string datasource, string userid, string pwd, string table)
 		{
-
+			SqlConnectionStringBuilder builder = new SqlConnectionStringBuilder();
+			builder.DataSource = datasource;
+			builder.UserID = userid;
+			builder.Password = pwd;
+			builder.InitialCatalog = table;
+			this.connection = new SqlConnection(builder.ConnectionString);
 		}
 
-		public Boolean connect()
+		public SqlConnection connect()
 		{
-
+			return this.connection;
 		}
 	}
 }
